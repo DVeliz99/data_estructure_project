0d3556b1089d2196e3d840f46cbf6e6651905342
DVeliz99 <velizalexander99@gmail.com>
2025-08-24 17:58:56 -0600

    feat(config.py-&-user.py): añadir .config y insertar usuario en tabla hash

--- main.py
+++ main.py
@@ -1,11 +1,21 @@
 from utils import hash, archive
+from models.user import User
 
 if __name__ == "__main__":
     # Parámetros de prueba
     size = 20
     nombre_archivo = "usuarios.txt"
     carpeta = "data"
+    
+    #usuario1 = User(
+    #id_usuario="12AB",
+    #nombre="Dario Veliz",
+    #correo="dario@example.com",
+    #acceso="2025-08-24 16:00"
+    #)
 
     # Inicializar la tabla hash en el archivo
     #hash.initialize_hash_table(size, nombre_archivo, carpeta)
-print(archive.is_address_empty(nombre_archivo, carpeta, hash.hash_function("80ZZ"))) 
+    #print(archive.is_address_empty(nombre_archivo, carpeta, hash.hash_function("80ZZ"))) 
+    # Insertar el usuario en la tabla hash
+    #usuario1.insert_user(usuario1.id_usuario, usuario1.nombre, usuario1.correo, usuario1.acceso)
---
+++ utils/config.py
@@ -0,0 +1,9 @@
+# utils/config.py
+
+# Carpeta donde están los datos
+DATA_FOLDER = "data"
+
+# Archivos de la tabla hash
+USUARIOS_FILE = "usuarios.txt"
+
+
