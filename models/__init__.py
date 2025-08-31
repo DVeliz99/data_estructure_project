0eb6a01aae8fd592e3831884bac9f8942e4f2a6c
DVeliz99 <velizalexander99@gmail.com>
2025-08-31 14:29:20 -0600

    --amend

--- models/__init__.py
+++ models/__init__.py
@@ -1,6 +1,5 @@
 #indica que la carpeta es un paquete
-from .access import push_id, pop_id
-from .user import insert_user,get_user_info
-#from .user_utils import metodos
+from .access import Access
+from .user import User
 
-__all__ = ["push_id", "pop_id","insert_user","get_user_info"]
+__all__ = ["Access", "User"]
